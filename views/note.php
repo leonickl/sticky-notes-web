3412310a5b9f5269682039804208d5ce98a21013
Leo Nickl <leo@leonickl.de>
2026-03-30 14:57:27 +0200

    close button

--- views/note.php
+++ views/note.php
@@ -1,9 +1,9 @@
 <div class="column-4 items-center">
     <div class="note wrapper note-<?= $note->style() ?> column-0">
-        <b class="header"><?= $note->title() ?></b>
+        <b class="header">
+            <a href="/notes" class="close text-center-both">&times;</a>
+        </b>
         <p class="content"><?= $note->content() ?></p>
         <em class="footer"><?= $note->modified() ?></em>
     </div>
-
-    <a href="/notes" class="btn w-10 text-center">Back</a>
 </div>
